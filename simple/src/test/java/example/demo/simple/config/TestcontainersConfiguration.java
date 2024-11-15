8db0d44fb83faf8497d1806cbd75c1de1027fff8
stepninkv <stepninkv@it.mos.ru>
2024-11-15 19:53:53 +0300

    исправил тест

--- simple/src/test/java/example/demo/simple/config/TestcontainersConfiguration.java
+++ simple/src/test/java/example/demo/simple/config/TestcontainersConfiguration.java
@@ -1,9 +1,5 @@
 package example.demo.simple.config;
 
-import com.github.dockerjava.api.model.ExposedPort;
-import com.github.dockerjava.api.model.HostConfig;
-import com.github.dockerjava.api.model.PortBinding;
-import com.github.dockerjava.api.model.Ports;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
@@ -25,8 +21,7 @@ public class TestcontainersConfiguration {
         return new OracleContainer(DockerImageName.parse("gvenzl/oracle-free:latest"))
                 .withUsername(USERNAME)
                 .withPassword(PASSWORD)
-                .withExposedPorts(PORT)
-                .withCreateContainerCmdModifier(cmd -> cmd.withHostConfig(new HostConfig().withPortBindings(new PortBinding(Ports.Binding.bindPort(PORT), new ExposedPort(PORT)))));
+                .withExposedPorts(PORT);
     }
 
 }
