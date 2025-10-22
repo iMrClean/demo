4acd0d3751fbc718b41a2f731303b4c815de5ed7
stepninkv <stepninkv@it.mos.ru>
2025-10-22 14:46:57 +0300

    fix multiple-db and route-multiple-db

--- multiple-db/src/test/java/example/demo/multiple/db/config/TestcontainersConfiguration.java
+++ multiple-db/src/test/java/example/demo/multiple/db/config/TestcontainersConfiguration.java
@@ -1,9 +1,9 @@
 package example.demo.multiple.db.config;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.oracle.OracleContainer;
 import org.testcontainers.utility.DockerImageName;
 import org.testcontainers.utility.MountableFile;
@@ -13,31 +13,15 @@ public class TestcontainersConfiguration {
 
   private static final String PRIMARY_USERNAME = "PRIMARY";
   private static final String PRIMARY_PASSWORD = "PRIMARY";
-  private static final String SECONDARY_USERNAME = "SECONDARY";
-  private static final String SECONDARY_PASSWORD = "SECONDARY";
 
   public static final OracleContainer CONTAINER = new OracleContainer(DockerImageName.parse("gvenzl/oracle-free:latest"))
-      .withEnv("APP_USER", PRIMARY_USERNAME)
-      .withEnv("APP_USER_PASSWORD", PRIMARY_PASSWORD)
-      .withUsername(PRIMARY_USERNAME)
-      .withPassword(PRIMARY_PASSWORD)
-      .withCopyFileToContainer(MountableFile.forClasspathResource("secondary-user.sql"), "/container-entrypoint-initdb.d/01-secondary-user.sql");
+          .withUsername(PRIMARY_USERNAME)
+          .withPassword(PRIMARY_PASSWORD)
+          .withCopyFileToContainer(MountableFile.forClasspathResource("secondary-user.sql"), "/container-entrypoint-initdb.d/01-secondary-user.sql")
+          .withLogConsumer(new Slf4jLogConsumer(LoggerFactory.getLogger("OracleContainer")));
 
   static {
     CONTAINER.start();
-  }
-
-  @DynamicPropertySource
-  static void registerDataSources(DynamicPropertyRegistry registry) {
-    // Primary datasource
-    registry.add("spring.datasource.primary.url", CONTAINER::getJdbcUrl);
-    registry.add("spring.datasource.primary.username", () -> PRIMARY_USERNAME);
-    registry.add("spring.datasource.primary.password", () -> PRIMARY_PASSWORD);
-
-    // Secondary datasource
-    registry.add("spring.datasource.secondary.url", CONTAINER::getJdbcUrl);
-    registry.add("spring.datasource.secondary.username", () -> SECONDARY_USERNAME);
-    registry.add("spring.datasource.secondary.password", () -> SECONDARY_PASSWORD);
   }
 
   @Bean
--- route-multiple-db/src/test/java/example/demo/route/multiple/db/config/TestcontainersConfiguration.java
+++ route-multiple-db/src/test/java/example/demo/route/multiple/db/config/TestcontainersConfiguration.java
@@ -1,9 +1,9 @@
 package example.demo.route.multiple.db.config;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.oracle.OracleContainer;
 import org.testcontainers.utility.DockerImageName;
 import org.testcontainers.utility.MountableFile;
@@ -13,38 +13,15 @@ public class TestcontainersConfiguration {
 
   private static final String PRIMARY_USERNAME = "PRIMARY";
   private static final String PRIMARY_PASSWORD = "PRIMARY";
-  private static final String SECONDARY_A_USERNAME = "SECONDARY-A";
-  private static final String SECONDARY_A_PASSWORD = "SECONDARY-A";
-  private static final String SECONDARY_B_USERNAME = "SECONDARY-B";
-  private static final String SECONDARY_B_PASSWORD = "SECONDARY-B";
 
   public static final OracleContainer CONTAINER = new OracleContainer(DockerImageName.parse("gvenzl/oracle-free:latest"))
-      .withEnv("APP_USER", PRIMARY_USERNAME)
-      .withEnv("APP_USER_PASSWORD", PRIMARY_PASSWORD)
-      .withUsername(PRIMARY_USERNAME)
-      .withPassword(PRIMARY_PASSWORD)
-      .withCopyFileToContainer(MountableFile.forClasspathResource("secondary-user.sql"), "/container-entrypoint-initdb.d/01-secondary-user.sql");
+          .withUsername(PRIMARY_USERNAME)
+          .withPassword(PRIMARY_PASSWORD)
+          .withCopyFileToContainer(MountableFile.forClasspathResource("secondary-user.sql"), "/container-entrypoint-initdb.d/01-secondary-user.sql")
+          .withLogConsumer(new Slf4jLogConsumer(LoggerFactory.getLogger("OracleContainer")));
 
   static {
     CONTAINER.start();
-  }
-
-  @DynamicPropertySource
-  static void registerDataSources(DynamicPropertyRegistry registry) {
-    // Primary datasource
-    registry.add("spring.datasource.primary.url", CONTAINER::getJdbcUrl);
-    registry.add("spring.datasource.primary.username", () -> PRIMARY_USERNAME);
-    registry.add("spring.datasource.primary.password", () -> PRIMARY_PASSWORD);
-
-    // Secondary-a datasource
-    registry.add("spring.datasource.secondary-a.url", CONTAINER::getJdbcUrl);
-    registry.add("spring.datasource.secondary-a.username", () -> SECONDARY_A_USERNAME);
-    registry.add("spring.datasource.secondary-a.password", () -> SECONDARY_A_PASSWORD);
-
-    // Secondary-b datasource
-    registry.add("spring.datasource.secondary-b.url", CONTAINER::getJdbcUrl);
-    registry.add("spring.datasource.secondary-b.username", () -> SECONDARY_B_USERNAME);
-    registry.add("spring.datasource.secondary-b.password", () -> SECONDARY_B_PASSWORD);
   }
 
   @Bean
